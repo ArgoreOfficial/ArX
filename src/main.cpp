29ac7215230f1fdac4bdb7687e0595ebc19e0a35
Argore <argoreyt@gmail.com>
2024-12-20 10:05:18 +0100

    typeval

--- src/main.cpp
+++ src/main.cpp
@@ -19,57 +19,81 @@ struct test_struct
 	void dothing2() {}
 };
 
-struct memvar_base { virtual void set( const char* ) = 0; };
+struct typed_obj {};
+struct member_base : public typed_obj { 
+	virtual void set( const char* ) = 0; 
+};
+
 template<typename _Ty>
-struct memvar : public memvar_base
+struct memvar : public member_base
 {
 	virtual void set( const char* _str ) override {
-		v = arg::str_to_T<_Ty>( _str );
+		//v = arg::str_to_T<_Ty>( _str );
 		printf( "%s\n", _str );
 	}
+};
 
-	_Ty v;
+template<typename _Ty, arg::typeval _Tval = arg::typeval_of<_Ty>::ty>
+struct ttest
+{
+	static const int a = 0;
 };
 
 template<typename _Ty>
-struct reflected_struct
+struct ttest<_Ty, arg::typeval::member_function>
 {
-	void set( const char* _member, const char* _value );
-
-private:
-	_Ty* m_pObj;
+	static const int a = 1;
 };
 
+
 template<typename _Ty, auto _Ty::* ..._Members>
-void refl_struct( const std::array<const char*, sizeof...( _Members )>& _names ) 
+struct reflected_struct : typed_obj
 {
-	std::vector<const char*> types{ arg::member_name<_Ty, _Members>()... };
-	//std::vector<memvar_base*> membs{ new memvar<arg::typename_of<decltype( _Members )>::Ty>()... };
+	reflected_struct( const std::array<const char*, sizeof...( _Members )>& _names ) {
+		std::vector<const char*> types{ arg::member_name<_Ty, _Members>()... };
+		//std::vector<member_base*> membs{ new memvar<arg::typename_of<decltype( _Members )>::Ty>()... };
+		std::vector<arg::typeval> realTypes{ arg::typeval_of<decltype( _Members )>::ty... };
+		std::vector<int> realTypes2{ ttest<decltype( _Members )>::a... };
 
-	for( size_t i = 0; i < sizeof...( _Members ); i++ )
-	{
-		printf( "%s %s::%s\n", types[ i ], typeid( _Ty ).name(), _names[ i ] );
+		for( size_t i = 0; i < sizeof...( _Members ); i++ )
+			printf( "%s %s::%s\n", types[ i ], typeid( _Ty ).name(), _names[ i ] );
 
-		//membs[ i ]->set( "3.3" );
 	}
-	
-}
+
+	// void set( const char* _member, const char* _value );
+	void* create( void* _pBase ) { _create( _pBase ) }
+
+private:
+	_Ty* _create( _Ty* _pBase = nullptr ) {
+		if( _pBase )
+			return new( _pBase ) _Ty{};
+		else
+			return new _Ty{};
+	}
+
+	_Ty* m_pObj{ nullptr };
+
+};
+
+int test2() { return 0; }
 
 int main()
 {
-	refl_struct <
-		test_struct,
-		&test_struct::member_int,
-		&test_struct::member_float,
-		&test_struct::member_char,
-		&test_struct::dothing2
-	>( { 
-		"member_int",
-		"member_float",
-		"member_char",
-		"dothing2"
-	   });
+	std::vector<arg::typeval> tps{
+		arg::typeval_of<decltype( &test_struct::dothing2 )>::ty,
+		arg::typeval_of<decltype( &test_struct::member_char )>::ty,
+		arg::typeval_of<int>::ty,
+		arg::typeval_of<decltype( &test2 )>::ty
+	};
 
+	reflected_struct<
+		test_struct, 
+		&test_struct::member_int, 
+		&test_struct::member_float, 
+		&test_struct::member_char,
+		&test_struct::dothing2> 
+	mystruct( { "member_int", "member_float", "member_char", "dothing2" });
+	
 	/*
 	test::test_unordered_array();
 	test::test_strong_type();
@@ -78,4 +102,3 @@ int main()
 
 	return 0;
 }
-
--- unordered_array/typename_of.hpp
+++ unordered_array/typename_of.hpp
@@ -21,6 +21,38 @@ constexpr const char* member_name() {
 	return typeid( typename_of<decltype( ptr )>::Ty ).name();
 }
 
+enum typeval {
+	type,
+	type_pointer,
+	function,
+	member_variable,
+	member_function,
+};
+
+template<typename _Ty> struct typeval_of {
+	static const typeval ty = type;
+};
+
+template<typename _Ty> struct typeval_of<_Ty*> : typeval_of<_Ty> {
+	static const typeval ty = type_pointer;
+};
+
+template<typename _Rty, typename... _Args>
+struct typeval_of<_Rty( * )( _Args... )> {
+	static const typeval ty = function;
+};
+
+template<typename _Ty, typename _Rty, typename... _Args>
+struct typeval_of<_Rty( _Ty::* )( _Args... )> {
+	static const typeval ty = member_function;
+};
+
+template<typename _Ty, typename _Mty>
+struct typeval_of<_Ty _Mty::*> {
+	static const typeval ty = member_variable;
+};
+
+
 
 template<typename T>
 T str_to_T( const char* _str ) {
@@ -30,5 +62,6 @@ T str_to_T( const char* _str ) {
 template<> const char* str_to_T<const char*>( const char* _str ) { return _str; }
 template<> int32_t     str_to_T<int32_t>    ( const char* _str ) { return std::stoi( _str ); }
 template<> float       str_to_T<float>      ( const char* _str ) { return std::stof( _str ); }
+
 
 }
