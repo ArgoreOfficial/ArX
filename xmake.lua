0bc9940189cb70a1dbcc314be02be11f2e9b241d
Argore <argoreyt@gmail.com>
2025-02-19 10:21:27 +0100

    rename project to libWyvern

--- xmake.lua
+++ xmake.lua
@@ -4,10 +4,10 @@ set_languages( "cxx17" )
 add_rules("mode.debug")
 add_rules("mode.release")
 
-target( "ArX" )
+target( "libWyvern" )
     set_kind( "binary" )
     
-    set_basename("ArX_test_$(mode)_$(arch)")
+    set_basename("libWyvern_test_$(mode)_$(arch)")
 
     set_targetdir "bin"
     set_objectdir "build/obj"
